3ac9f0e8237338a660f9278f444c9a9477eaa1bb
Lucas  Araújo <112007865+Lucas-resende21@users.noreply.github.com>
2023-04-04 17:41:20 +0000

    início da classe aeronave

---
+++ class.Aeronave.php
@@ -0,0 +1,13 @@
+<?php
+  class Aeronave{
+    private $fabricante; 
+    private $modelo;
+    private $capPassageiros;
+    private $capCarga;
+    private $registro;
+
+    public function __construct(string $_fab, string $_mod, int $_capPassageiros, float $_capCarga, string $_reg){
+      
+    }
+    public function __destruct(){}
+  }
