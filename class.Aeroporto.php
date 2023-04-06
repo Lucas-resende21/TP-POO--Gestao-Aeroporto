cba5acd7e3f20a4d0d97996c0c69bcc0bb5df7a0
danieldmmaforte <90412995+DanielMaforte@users.noreply.github.com>
2023-04-06 22:30:38 +0000

    implementação aeroporto

--- class.Aeroporto.php
+++ class.Aeroporto.php
@@ -1,1 +1,10 @@
 <?php
+  class Aeroporto{
+    protected $sigla;
+    protected $cidadeUf;
+    public function __construct(string $_sigla, string $_cidadeUf){
+      $this->sigla = $_sigla;
+      $this->cidadeUf = $_cidadeUf;
+    }
+    public function __destruc(){}
+  }
