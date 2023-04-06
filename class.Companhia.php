d54ad7a77e208f48a24469394cba519617ae9977
Luiz Kevenin <112576124+lKevenin@users.noreply.github.com>
2023-04-06 23:22:01 +0000

    criação da class.Companhia

--- class.Companhia.php
+++ class.Companhia.php
@@ -1,13 +1,40 @@
 <?php
+  include_once("class.Aeronave.php")
+  
   class Companhia{
-    private $nome;
-    private $codigo;
-    private $razsocial;
-    private $CNPJ;
-    private $sigla;
-    private $aeronaves;
-
-    public function __construct()
+    
+    private $_nome;
+    private $_codigo;
+    private $_razsocial;
+    private $_CNPJ;
+    private $_sigla;
+    private $_aeronaves = array();
 
     
+    public function __construct(string $nome, string $cod, string $razsocial, int $CNPJ, string $sigla) {
+      
+      $this->_nome = $nome;
+      $this->_codigo = $cod;
+      $this->_razsocial = $razsocial;
+      $this->_CNPJ = $CNPJ;
+      $this->_sigla = $sigla;
+    }
+
+    public function adicionaAeronave(string $fab, string $mod, int $capPsg, float $capCarg, string $reg) {
+
+      $aeronave = new Aeronave($fab, $mod, $capPsg, $capCarg, $reg);
+      $this->_aeronaves[] = $aeronave;
+    }
+
+    public function removeAeronave(Aeronave $aeronave) 
+
+      for(i = 0; i < sizeof($aeronaves()); i++) {
+          
+          if($aeronaves[i] == $aeronave)  {
+            unset($this->aeronaves[i]);
+          }
+      }
+    }
+
+    public function 
   }
