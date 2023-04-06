d1957f5d049afb48e79f0810700bd44484731b88
Lucas  Araújo <112007865+Lucas-resende21@users.noreply.github.com>
2023-04-06 01:02:27 +0000

    Finalização de aeronave e começo de companhia

--- class.Aeronave.php
+++ class.Aeronave.php
@@ -1,13 +1,17 @@
-<?php
+<?php  
   class Aeronave{
     private $fabricante; 
     private $modelo;
     private $capPassageiros;
     private $capCarga;
     private $registro;
-
     public function __construct(string $_fab, string $_mod, int $_capPassageiros, float $_capCarga, string $_reg){
-      
+      $this->fabricante = $_fab;
+      $this->modelo = $_mod;
+      $this->capPassageiros = $_capPassageiros;
+      $this->capCarga = $_capCarga;
+      $this->registro = $_reg;
     }
     public function __destruct(){}
   }
+  
--- class.Companhia.php
+++ class.Companhia.php
@@ -1,1 +1,13 @@
+<?php
+  class Companhia{
+    private $nome;
+    private $codigo;
+    private $razsocial;
+    private $CNPJ;
+    private $sigla;
+    private $aeronaves;
 
+    public function __construct()
+
+    
+  }
