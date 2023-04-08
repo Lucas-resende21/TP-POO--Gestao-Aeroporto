02d17c980178ccd4be4cdb6106c47863c68cd46d
Lucas  Araújo <112007865+Lucas-resende21@users.noreply.github.com>
2023-04-08 02:59:03 +0000

    Verificando que o registro de aeronave está no valor especificado no enunciado parte 1

--- class.Aeronave.php
+++ class.Aeronave.php
@@ -6,6 +6,28 @@
     private $capCarga;
     private $registro;
     public function __construct(string $_fab, string $_mod, int $_capPassageiros, float $_capCarga, string $_reg){
+      if(strlen($_reg)>6){
+        print_r("Registro inválido, inserir menor");
+        echo ("</p>");
+        return("Objeto não foi construido");}
+      elseif(strlen($_reg)<6){
+        print_r("Registro inválido, inserir maior");
+        return("Objeto não foi construido");}
+      if($_reg[2]!='-'){
+        print_r("Registro inválido, inserir hifen");
+        echo ("</p>");
+        return("Objeto não foi construido");}
+      if($_reg[0]!='P'){
+        print_r("Prefixo do registro invalido");
+        echo ("</p>");
+        return("Objeto não foi construido");
+      }
+      if($_reg[1]!='T'&&$_reg[1]!='R'&&$_reg[1]!='S'&&$_reg[1]!='P'){
+        print_r("Prefixo do registro invalido");
+        echo ("</p>");
+        return("Objeto não foi construido");
+      }
+        
       $this->fabricante = $_fab;
       $this->modelo = $_mod;
       $this->capPassageiros = $_capPassageiros;
@@ -14,4 +36,3 @@
     }
     public function __destruct(){}
   }
-  
