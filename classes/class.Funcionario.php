4624856d4f6c08202dbbbd14443906c99dfbdea1
DanielMaforte <90412995+DanielMaforte@users.noreply.github.com>
2023-05-18 04:18:31 +0000

    herda informações de pessoa em funcionario, também implementa setCHT e setEndereco em funcionario

--- classes/class.Funcionario.php
+++ classes/class.Funcionario.php
@@ -1,22 +1,43 @@
 <?php
-  include_once("class.Pessoa.php");
+include_once("class.Pessoa.php");
 
-class Funcionario extends Pessoa{
-    static $local_filename = "funcionario.txt";
-    static public function getFilename() {
-    return get_called_class()::$local_filename;
-    }
-    
-    private $CPF;
-    private $nacionalidade;
-    private $dataDeNascimento;
-    private $email;
+class Funcionario extends Pessoa {
     private $CHT;
     private $endereco;
     private $companhiaAerea;
     private $aeroportoBase;
 
-    public function getEndereço(){
-      return($this->endereco);
+    public function __construct($_nome, $_documento, $_CPF, $_nacionalidade, $_dataDeNascimento, $_email, $_CHT, $_endereco, $_companhiaAerea, $_aeroportoBase) {
+        parent::__construct($_nome, $_documento, $_CPF, $_nacionalidade, $_dataDeNascimento, $_email);
+        $this->setCHT($_CHT);
+        $this->setEndereco($_endereco);
+        $this->companhiaAerea = $_companhiaAerea;
+        $this->aeroportoBase = $_aeroportoBase;
+    }
+
+    public function getCHT() {
+      return $this->CHT;
+    }
+
+    public function setCHT($_CHT){
+      //implementar validação da CHT
+      $this->CHT = $_CHT;
+    }
+  
+    public function getEndereco() {
+      return $this->endereco;
+    }
+
+    public function setEndereco($_endereco){
+      //implementar validação do endereço
+      $this->endereco = $_endereco;
+    }
+
+    public function getCompanhiaAerea() {
+      return $this->companhiaAerea;
+    }
+
+    public function getAeroportoBase() {
+      return $this->aeroportoBase;
     }
 }
