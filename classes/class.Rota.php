882dd6a19ba70a481cf07fc67f810faaa1821f1d
Lucas-resende21 <112007865+Lucas-resende21@users.noreply.github.com>
2023-05-18 17:20:58 +0000

    rota pt3

--- classes/class.Rota.php
+++ classes/class.Rota.php
@@ -16,10 +16,15 @@
     public function __construct($_endereço, $_aeroporto){
       $this->Aeroporto = $_aeroporto;
       $this->trajeto[] = $_endereço;
+      // resolver a questão de colocar o endereço do aeroporto na matriz de endereços e adequar dnv os 
+      // outros endereços no programa dessa vez p serem um array(coordx, coordy)
     }
 
     public function addEndereço($_coordx, $_coordy){
-      
+      $coords = array($_coordx, $_coordy);
+      $this->trajeto[] = $coords();
+      $a = count($this->trajeto);
+      $this->distancia = $this->distancia + $this->CalculaDistancia($this->trajeto($a)(0),$this->trajeto($a)(1),$_coordx, $_coordy);
     }
     public function CalculaDistancia($_x1, $_y1, $_x2, $_y2){
       return(110.57*sqrt(pow($_x2-$_x1,2)+pow($_y2-$_y1)));
