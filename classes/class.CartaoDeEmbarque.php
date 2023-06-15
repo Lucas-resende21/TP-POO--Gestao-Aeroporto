096023cf726a03a77eb3aa5ad50b7415340d4354
IsabelSoier <131410276+IsabelSoier@users.noreply.github.com>
2023-06-15 22:38:50 +0000

    Cartão de embarque

---
+++ classes/class.CartaoDeEmbarque.php
@@ -0,0 +1,29 @@
+<?php  
+
+include_once("passageiro.php");
+include_once("class.Voo.php");
+include_once("persist.php");
+
+public function setNome($_nome){
+  echo $this->_nome;
+}
+
+public function setOrigem($origem){
+  print ($origem);
+}
+
+public function setDestino($destino){
+  print ($destino);
+}
+
+public function setHorarioEmbarque($embarque){
+  print ($embarque)
+}
+
+public function setHorarioPartida($_Viagem){  
+   print ($_Viagem->getHorarioP());
+}
+
+public function setAssento($assento){
+  print ($assento);
+}
