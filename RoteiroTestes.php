ee1264899e3feb60154199379eeab2cc8b24ff1e
Lucas-resende21 <112007865+Lucas-resende21@users.noreply.github.com>
2023-06-16 12:30:16 +0000

    .

--- RoteiroTestes.php
+++ RoteiroTestes.php
@@ -1,7 +1,7 @@
 <?php
   require_once('global.php');
     if( 1 ){
-      $sistema = new Sistema();
+      $sistema = Sistema::getInstance();
       $sistema->setUser("Lucas", "abacate", "lucasaraujoresende21@gmail.com");
       $sistema->login("Lucas", "abacate");
 
@@ -41,7 +41,7 @@
       echo("Voos criados ok");
       echo("\n");
 
-      $sistema->criaCalendário("1", "30");
+      $sistema->criaCalendario();
 
       echo("Calendario criado");
       echo("\n");
