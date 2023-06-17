f72a33ab64ae527878e5cc81fe7002c6c6661a17
Lucas-resende21 <112007865+Lucas-resende21@users.noreply.github.com>
2023-06-17 22:15:30 +0000

    ,

--- classes/class.Rota.php
+++ classes/class.Rota.php
@@ -32,7 +32,7 @@
         'destination' => $this->trajeto[$i+1], // Ufmg
         'key' => 'AIzaSyDptEOEPM1XmE6FTdEs3UpiJR-yAaI0krA'
       );
-      $url .= '?' . http_build_query($params);
+      $url .= '?' . +http_build_query($params);
       $curl = curl_init();
       curl_setopt($curl, CURLOPT_URL, $url);
       curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
